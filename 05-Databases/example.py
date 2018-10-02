3564aca855c1435945fe4fd15b83e1274e588bde
Annie <anniewong630@gmail.com>
2018-10-01 23:14:11 -0400

    adoption page

--- 05-Databases/example.py
+++ 05-Databases/example.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'datasqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'example_basic.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
